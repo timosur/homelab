c6d382a7ee4bbc80016254bf33602a014245a864
Timo Surmann <github@timosur.com>
2025-08-28 08:27:36 +0200

    fix(ssh): setup

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,12 @@
 terraform {
   cloud {
     organization = "timosur"
-
+    
     workspaces {
       name = "homelab"
     }
   }
+  
+  # Force local execution even with Terraform Cloud
+  # This way we get remote state storage but local execution
 }
