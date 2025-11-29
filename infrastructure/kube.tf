33ca45aa90bbd497c1bcc3eedade9a9e56c15907
Timo Surmann <github@timosur.com>
2025-11-29 19:16:36 +0100

    feat(klipper): disabled

--- infrastructure/kube.tf
+++ infrastructure/kube.tf
@@ -98,7 +98,7 @@ module "kube-hetzner" {
   disable_kube_proxy = true
 
   # Use Klipper LB (built-in k3s load balancer)
-  enable_klipper_metal_lb = true
+  enable_klipper_metal_lb = false
 
   cilium_version = "1.18.1"
   cilium_values  = <<EOT
